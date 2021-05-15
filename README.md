14ee6ad9eef388099835a3fa88af0229a00328c6
hanchen <hanchenjhc@gmail.com>
2021-05-15 15:06:14 -0500

    Externalize database configuration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Backend for cooperative Sudoku.
+
+To connect to docker MongoDB: docker exec -it mongo1 mongo
+
+[mongo shell command reference](https://docs.mongodb.com/manual/reference/mongo-shell/)
--- src/main/java/hanchen/springpusheventtoy/MongoReactiveConfiguration.java
+++ src/main/java/hanchen/springpusheventtoy/MongoReactiveConfiguration.java
@@ -1,21 +1,28 @@
 package hanchen.springpusheventtoy;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.context.annotation.Configuration;
 
-@EnableReactiveMongoRepositories
-public class MongoReactiveConfiguration extends AbstractReactiveMongoConfiguration {
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+@Configuration
+public class MongoReactiveConfiguration {
 
     @Bean
-    public MongoClient mongoClient() {
-        return MongoClients.create();
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return "reactive";
+    public MongoClient mongoClient(@Value("${spring.data.mongodb.uri}") String connectionString) {
+        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+        return MongoClients.create(MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .codecRegistry(codecRegistry)
+                .build());
     }
 }
